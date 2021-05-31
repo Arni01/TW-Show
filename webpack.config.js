386bd8d492aaffd6f683af1295c30ebc9f4becca
BSVT-NT\Mucha <at@at.com>
2021-05-31 16:37:22 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -54,9 +54,9 @@ const plugins = () => {
     }),
   ];
 
-  if (isProd) {
-    base.push(new BundleAnalyzerPlugin());
-  }
+  // if (isProd) {
+  //   base.push(new BundleAnalyzerPlugin());
+  // }
   return base;
 };
 
